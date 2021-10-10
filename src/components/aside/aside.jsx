16f304b7fb2e5ac7ad6a8b1887d8dc280ae7bac3
AllanDiogoDev <89414789+AllanDiogoDev@users.noreply.github.com>
2021-10-10 11:23:38 -0300

    10/10/21

--- src/components/aside/aside.jsx
+++ src/components/aside/aside.jsx
@@ -5,13 +5,25 @@ export default  function Aside(){
 
     return (  
         <aside>
-            <div className="container-aside"> 
-                <h2> Fique por dentro de todas ofertas com exclusividade! </h2>
-                <form className="form"> 
-                    <input className="pesquisa-input" type="text" placeholder="Nome" />
-                    <input className="pesquisa-input" type="text" placeholder="Email" />
-                    <button className="busca-input" >Aceito!</button>
-                </form>
+            <div className="container-aside">
+                <div className="display-flex">
+                    <form className="form"> 
+                        <h2> Fique por dentro de todas ofertas com exclusividade! </h2>
+                        <a href>
+                            <img className="icon-input" src="./imgs/user-icon.png" alt="icone usuario" />
+                            <input className="pesquisa-input" type="text" placeholder="Nome" />
+                        </a>
+                        <a href>
+                            <img className="icon-input" src="./imgs/email-icon.png" alt="icone email" />
+                            <input className="pesquisa-input" type="text" placeholder="Email" />
+                        </a>
+                        <button className="busca-input" >Aceito!</button>
+                    </form>
+                    <figure>
+                        <img className="jacare" src="./imgs/jacare-icon-celular.png" alt="Icone Carrinho de Compras"/>
+                        <figcaption class="hidden"> Icone Carrinho de Compras </figcaption>
+                    </figure>
+                </div>  
             </div>
         </aside>
     );
