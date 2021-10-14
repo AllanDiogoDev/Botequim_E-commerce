872b54110ce73f8dabe230b954f508dbab40eb78
AllanDiogoDev <89414789+AllanDiogoDev@users.noreply.github.com>
2021-10-14 00:59:40 -0300

    aa

--- src/components/aside/aside.jsx
+++ src/components/aside/aside.jsx
@@ -6,24 +6,22 @@ export default function Aside(){
     return (  
         <aside>
             <div className="container-aside">
-                <div className="display-flex">
-                    <form className="form"> 
-                        <h2> Fique por dentro de todas ofertas com exclusividade! </h2>
-                        <a href>
-                            <img className="icon-input" src="./imgs/user-icon.png" alt="icone usuario" />
-                            <input className="pesquisa-input" type="text" placeholder="Nome" />
-                        </a>
-                        <a href>
-                            <img className="icon-input" src="./imgs/email-icon.png" alt="icone email" />
-                            <input className="pesquisa-input" type="text" placeholder="Email" />
-                        </a>
-                        <button className="busca-input" >Aceito!</button>
-                    </form>
-                    <figure>
-                        <img className="jacare" src="./imgs/jacare-icon-celular.png" alt="Icone Carrinho de Compras"/>
-                        <figcaption class="hidden"> Icone Carrinho de Compras </figcaption>
-                    </figure>
-                </div>  
+                <form className="form"> 
+                    <h2> Fique por dentro de todas ofertas com exclusividade! </h2>
+                    <a href>
+                        <img className="icon-input" src="./imgs/user-icon.png" alt="icone usuario" />
+                        <input className="pesquisa-input" type="text" placeholder="Nome" />
+                    </a>
+                    <a href>
+                        <img className="icon-input" src="./imgs/email-icon.png" alt="icone email" />
+                        <input className="pesquisa-input" type="text" placeholder="Email" />
+                    </a>
+                    <button className="busca-input" >Aceito!</button>
+                </form>
+                <figure>
+                    <img className="jacare" src="./imgs/jacare-icon-celular.png" alt="Icone Carrinho de Compras"/>
+                    <figcaption class="hidden"> Icone Carrinho de Compras </figcaption>
+                </figure>
             </div>
         </aside>
     );
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,42 +4,41 @@ import './header.css';
 export default function Header(){
     return ( 
         <header id="botequim">
-            <div className="header-container">
+            <figure>
+                <img className="logo-botequim" src="./imgs/Botequim-Logo.png" alt="Logo Botequim"/>
+                <figcaption class="hidden"> Logo Botequim </figcaption>
+            </figure>
+
+            <nav>
+                <ul>
+                    <li>
+                        <a href="$"> Sobre </a>
+                    </li>
+                    
+                    <li>
+                        <a href="$"> Fornecedores </a>
+                    </li>
+
+                    <li>
+                        <a href="$"> Ofertas</a>
+                    </li>
+
+                    <li>
+                        <a href="$"> Atend. Clientes </a>
+                    </li>
+                </ul>  
+            </nav>
+
+            <div className="icones">
                 <figure>
-                    <img className="logo-botequim" src="./imgs/Botequim-Logo.png" alt="Logo Botequim"/>
-                    <figcaption class="hidden"> Logo Botequim </figcaption>
+                    <img className="itens-menu" src="./imgs/carrinho.png" alt="Icone Carrinho de Compras"/>
+                    <figcaption class="hidden"> Icone Carrinho de Compras </figcaption>
                 </figure>
-
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="$"> Sobre </a>
-                        </li>
-                        
-                        <li>
-                            <a href="$"> Fornecedores </a>
-                        </li>
-
-                        <li>
-                            <a href="$"> Ofertas</a>
-                        </li>
-
-                        <li>
-                            <a href="$"> Atend. Clientes </a>
-                        </li>
-                    </ul>  
-                </nav>
-                <div className="icones">
-                    <figure>
-                        <img className="itens-menu" src="./imgs/carrinho.png" alt="Icone Carrinho de Compras"/>
-                        <figcaption class="hidden"> Icone Carrinho de Compras </figcaption>
-                    </figure>
-                        
-                    <figure>
-                        <img className="itens-menu2" src="./imgs/entrar.png" alt="Icone Entrar"/>
-                        <figcaption class="hidden"> Icone Entrar na sua Conta</figcaption>
-                    </figure>  
-                </div>
+                    
+                <figure>
+                    <img className="itens-menu2" src="./imgs/entrar.png" alt="Icone Entrar"/>
+                    <figcaption class="hidden"> Icone Entrar na sua Conta</figcaption>
+                </figure>  
             </div>
             <a className="btn" href="#botequim"></a>
         </header>
